7f3669b5f36c8dbf4844ad345f2f8b927c3fb298
KoyomiHidaka <abbishev@mail.ru>
2024-08-02 16:40:33 +0500

    Clean

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = "7197317607:AAF_8zHyYlU7w-o-fGfawLLson07gkUI5N4"
-
-
--- config_reader.py
+++ config_reader.py
@@ -13,6 +13,11 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
+
+
+
+
+
 # При импорте файла сразу создастся
 # и провалидируется объект конфига,
 # который можно далее импортировать из разных мест
